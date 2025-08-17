1acf1431c3f8de47dfd58dea8ec9b83e65e4651b
qwer1304 <qwer1304@hotmail.com>
2025-08-17 15:33:50 +0300

    implement loading from trained linear

--- linear.py
+++ linear.py
@@ -39,7 +39,7 @@ class Net(nn.Module):
         else:
             model.module.fc = nn.Linear(2048, num_class, bias=True)
             msg = model.load_state_dict(state_dict, strict=False)
-            print([k for k in msg.missing_keys if 'g.' not in k], msg.missing_keys)
+            print([k for k in msg.unexpected_keys if 'g.' not in k], msg.missing_keys)
             self.fc = model.module.fc
 
     def forward(self, x):
