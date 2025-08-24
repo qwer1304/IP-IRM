6bf9303cf81db2168fe99e94d5d817f0db9737b3
qwer1304 <qwer1304@hotmail.com>
2025-08-24 08:13:16 +0300

    add resize

--- resizeimgs.py
+++ resizeimgs.py
@@ -4,7 +4,7 @@ import argparse
 
 def scantree(path):
     """Recursively yield DirEntry objects for given directory."""
-    for entry in scandir(path):
+    for entry in os.scandir(path):
         if entry.is_dir(follow_symlinks=False):
             yield from scantree(entry.path)
         elif entry.is_file():
