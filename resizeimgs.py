b8f9118ea75484f713578a8c4b4105aa0c5ea642
qwer1304 <qwer1304@hotmail.com>
2025-08-26 12:01:35 +0300

    add tqdm to resize

--- resizeimgs.py
+++ resizeimgs.py
@@ -1,6 +1,16 @@
 import os
 from PIL import Image
 import argparse
+import tqdm
+
+def count_files(path):
+    total = 0
+    for entry in os.scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            total += count_files(entry.path)
+        elif entry.is_file():
+            total += 1
+    return total
 
 def scantree(path, progress=False, level=0):
     """Recursively yield DirEntry objects for given directory."""
@@ -20,42 +30,43 @@ def path_from_depth(path, depth):
 def main(args):
 
     size = args.target_image_size
+    total_files = count_files(args.in_dir)
     print("Begin conversion...")
-    for infile in scantree(args.in_dir, progress=args.progress, level=0):
+    with tqdm(total=total_files, unit="file", desc="Processing files") as pbar:
+        for infile in scantree(args.in_dir, progress=args.progress, level=0):
 
-        fnext = infile.name # with file ext
-        fn, fext = os.path.splitext(fnext) # fext has the '.'
-        relfnext = path_from_depth(infile.path, args.depth)
-        outfile = os.path.join(args.out_dir, relfnext)
-        if args.out_enc == "WEBP":
-            outfile = os.path.splitext(outfile)[0] + ".webp"
-        if args.skip_existing and os.path.exists(outfile):
-            continue
-        outpath = os.path.dirname(outfile)
-        os.makedirs(outpath, exist_ok=True) # better safe than sorry
+            fnext = infile.name # with file ext
+            fn, fext = os.path.splitext(fnext) # fext has the '.'
+            relfnext = path_from_depth(infile.path, args.depth)
+            outfile = os.path.join(args.out_dir, relfnext)
+            if args.out_enc == "WEBP":
+                outfile = os.path.splitext(outfile)[0] + ".webp"
+            if args.skip_existing and os.path.exists(outfile):
+                continue
+            outpath = os.path.dirname(outfile)
+            os.makedirs(outpath, exist_ok=True) # better safe than sorry
 
-        #print(fnext, outfile)
-      
-        if (infile.path != outfile):
-            try:
-                im = Image.open(infile.path)
-            except IOError:
-                print("cannot open '%s'" % infile.path)
-            try:
-                w, h = im.size
-                scale = size / max(w, h)
-                new_size = (int(round(w * scale)), int(round(h * scale)))
-                im.resize(new_size, Image.Resampling.LANCZOS)
-            except IOError:
-                print(f"cannot resize {infile.path} to ({w},{h})")
-            try:
-                if args.out_enc == "WEBP":
-                    im.save(outfile, "WEBP", quality=95, subsampling=0, optimize=True)
-                else:
-                    im.save(outfile, "JPEG", quality=95, subsampling=0, optimize=True) # overwrites file if it exists
-            except IOError:
-                print(f"cannot save {infile.path} into {outfile}")
-    print("Done!")  
+            if (infile.path != outfile):
+                try:
+                    im = Image.open(infile.path)
+                except IOError:
+                    print("cannot open '%s'" % infile.path)
+                try:
+                    w, h = im.size
+                    scale = size / max(w, h)
+                    new_size = (int(round(w * scale)), int(round(h * scale)))
+                    im.resize(new_size, Image.Resampling.LANCZOS)
+                except IOError:
+                    print(f"cannot resize {infile.path} to ({w},{h})")
+                try:
+                    if args.out_enc == "WEBP":
+                        im.save(outfile, "WEBP", quality=95, subsampling=0, optimize=True)
+                    else:
+                        im.save(outfile, "JPEG", quality=95, subsampling=0, optimize=True) # overwrites file if it exists
+                except IOError:
+                    print(f"cannot save {infile.path} into {outfile}")
+            pbar.update(1)
+        print("Done!")  
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
