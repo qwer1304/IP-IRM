c4ca38fbc4ac3139347d28fb78032a248c353064
qwer1304 <qwer1304@hotmail.com>
2025-09-09 17:43:09 +0300

    add restore messages

--- main.py
+++ main.py
@@ -736,9 +736,11 @@ def load_checkpoint(path, model, model_momentum, optimizer, device='cuda'):
 
     # Restore RNG states
     rng_dict = checkpoint['rng_dict']
+    if rng_state.device != torch.device('cpu'):
+        rng_state = rng_state.cpu()   
     torch.set_rng_state(rng_dict['rng_state'])
-    if rng_dict['cuda_rng_state'] is not None and torch.cuda.is_available():
-        torch.cuda.set_rng_state_all(rng_dict['cuda_rng_state'])
+    if rng_dict['cuda_rng_state'] is not None:
+        torch.cuda.set_rng_state_all([t.cpu() if t.device != torch.device('cpu') else t for t in rng_dict['cuda_rng_state']])
     np.random.set_state(rng_dict['numpy_rng_state'])
     random.setstate(rng_dict['python_rng_state'])
 
