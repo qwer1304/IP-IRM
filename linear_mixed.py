e5149f483eb3c44af7d0200930abc78a2b5d9a3c
qwer1304 <qwer1304@hotmail.com>
2025-09-22 09:56:55 +0300

    add mixup to linear probe

--- linear_mixed.py
+++ linear_mixed.py
@@ -199,6 +199,7 @@ def train_val(net, data_loader, train_optimizer, batch_size, args, dataset="test
                     feature = torch.cat(feature_list, dim=0)
                     target = torch.cat(target_list, dim=0)
                     feature_mixed, labels_mixed = mixup(feature, target})
+                    feature_mixed = feature_mixed.squeeze()
                     out = net.fc(feature_mixed)
                     def loss_mixup(y, logits):
                         loss_a = criterion(logits, y[:, 0].long(), reduction='none')
