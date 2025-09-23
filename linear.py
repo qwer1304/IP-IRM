ed001ff8532b219c36fc3cedf79593d746a7c94e
qwer1304 <qwer1304@hotmail.com>
2025-09-23 16:16:59 +0300

    standard resent

--- linear.py
+++ linear.py
@@ -221,7 +221,9 @@ def train_val(net, data_loader, train_optimizer, batch_size, args, dataset="test
 
                         loss = loss_mixup(labels_mixed, out)
                     else:
-                        out = net.module.fc(net.module.dropout(feature.squeeze()))
+                        out = net.module.fc(net.module.dropout(feature))
+                        print()
+                        print(type(out), out.size, type(target), target.size())
                         loss = loss_mixup_criterion(target, out).mean()
                     loss.backward()
             
