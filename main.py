ea5de22a45d5146c2132434eee61addcc5aef8a0
qwer1304 <qwer1304@hotmail.com>
2025-09-27 16:55:50 +0300

    vectorized

--- main.py
+++ main.py
@@ -257,9 +257,7 @@ class CE_IRMCalculator(IRMCalculator):
         device = self.loss_module.logits().device
         # one scalar (requires grad)
         batch_size = self.loss_module.logits(idxs=idxs).size(0)
-        print()
-        print(f"batch_size: {batch_size}")
-        s = torch.ones(batch_size, device=device, requires_grad=True)  # one s per sample
+        s = torch.ones(batch_size, 1, device=device, requires_grad=True)  # one s per sample
         # Compute g_i in a CE-specific way
 
         losses = self.loss_module.compute_loss_micro(idxs=idxs, scale=s, temperature=self.irm_temp, **kwargs)
