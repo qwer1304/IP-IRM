0df99d6eff24078ead46d6676a07aa9ccd164564
qwer1304 <qwer1304@hotmail.com>
2025-10-05 15:39:18 +0300

    gradnorm

--- main.py
+++ main.py
@@ -840,6 +840,8 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
                 
                 loss_plus_keep_weight = sum([normalized_weights[k] for k in normalized_weights if k != 'penalty'])
                 tau                   = loss_plus_keep_weight / (normalized_weights['penalty'] + 1e-12)
+                print()
+                print([f'{k}: {v:.2e}' for k,v in enumerate(normalized_weights)])
         
         loss_keep_grad_scaler = normalized_weights['loss_keep'] if 'loss_keep' in normalized_weights else torch.tensor(1.0, dtype=torch.float, device=device)
         loss_grad_scaler      = normalized_weights['loss']      if 'loss'      in normalized_weights else torch.tensor(1.0, dtype=torch.float, device=device)
