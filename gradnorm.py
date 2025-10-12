6b62d19cbd3ce7dd9a80f0c7c439b38b08850a7d
qwer1304 <qwer1304@hotmail.com>
2025-10-12 09:47:31 +0300

    add gradnorm tau

--- gradnorm.py
+++ gradnorm.py
@@ -169,12 +169,12 @@ class GradNormLossBalancer(nn.Module):
             rates = smoothed_rates.detach()  
 
             r = (veights * g) - (avgG * rates)           # residuals r_i
-            global_term = (r * rates).mean()             # (1/N) sum_j r_j * rate_j
             if self.gradnorm_loss_type == 'L1':
-                s = r.sign()                                 # s_i = sign(res_i)
-                global_term = (s * rates).mean()             # (1/N) sum_i s_i * rho_i
-                expected_v_grad = self.Gscaler * g * (r - global_term) / T
+                s = r.sign()                             # s_i = sign(res_i)
+                global_term = (s * rates).mean()         # (1/N) sum_i s_i * rho_i
+                expected_v_grad = self.Gscaler * g * (s - global_term) / T
             elif self.gradnorm_loss_type == 'L2':
+                global_term = (r * rates).mean()         # (1/N) sum_j r_j * rate_j
                 expected_v_grad = self.Gscaler * 2.0 * g * (r - global_term) / T
 
             with np.printoptions(precision=6):
