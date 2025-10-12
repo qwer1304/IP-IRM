bdf535ab3840e726f39ce795cefd364516da9c40
qwer1304 <qwer1304@hotmail.com>
2025-10-12 09:14:51 +0300

    add gradnorm tau

--- gradnorm.py
+++ gradnorm.py
@@ -143,7 +143,7 @@ class GradNormLossBalancer(nn.Module):
         to learn relative scales. The GradNorm loss must be unconstrained, otherwise the model can't freely adjust magnitudes.
         Normalization is only applied after the update, when you want to use the weights to combine task losses in the forward pass.
         """
-        gradnorm_loss = self.Gscaler * (weighted_grad_norms - avgG_semi_detached * smoothed_rates).abs().mean()
+        gradnorm_loss = self.Gscaler * (weighted_grad_norms - avgG_semi_detached * smoothed_rates).abs().sum()
         #gradnorm_loss = self.Gscaler * ((weighted_grad_norms - avg_grad_norm * smoothed_rates) ** 2).mean()
 
         # Step 6: Normalize task weights
@@ -168,7 +168,7 @@ class GradNormLossBalancer(nn.Module):
 
             r = (veights * g) - (avgG * rates)           # residuals r_i
             global_term = (r * rates).mean()             # (1/N) sum_j r_j * rate_j
-            expected_v_grad = self.Gscaler * g * (r - global_term).sign() / T
+            expected_v_grad = self.Gscaler * g * (r - global_term).sign()
             #expected_v_grad = self.Gscaler * 2.0 * g * (r - global_term) / T
 
             with np.printoptions(precision=6):
