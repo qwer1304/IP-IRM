8c72eb4fb7ca90df080ad03d5c939e4d568b76b3
qwer1304 <qwer1304@hotmail.com>
2025-10-13 07:54:39 +0300

    add gradnorm diagnostics / warnings

--- gradnorm.py
+++ gradnorm.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import warnings
 
 class GradNormLossBalancer(nn.Module):
     def __init__(self, initial_weights, alpha=1.2, device='cpu', smoothing=False, tau=None, eps=1e-8, debug=False, 
