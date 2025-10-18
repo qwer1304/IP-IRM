0c43f4e3a614aa005ece64fa902cdf4663339ce1
qwer1304 <qwer1304@hotmail.com>
2025-10-18 15:43:51 +0300

    debug bad pen grads

--- main.py
+++ main.py
@@ -1029,23 +1029,25 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
         penalty_weighted   *= penalty_grad_scaler 
         """
 
-        print()
+        if args.debug:
+            print()
         for pind, p in enumerate(net.parameters()):        
             total_grad_flat_weighted = (  loss_keep_grads_final[pind] * loss_keep_weight * loss_keep_grad_scaler
                                         + loss_grads_final[pind]      * loss_weight      * loss_grad_scaler
                                         + penalty_grads_final[pind]   * penalty_weight   * penalty_grad_scaler
                                        )
 
-            g_L = loss_grads_final[pind]      * loss_weight      * loss_grad_scaler
-            g_P = penalty_grads_final[pind]   * penalty_weight   * penalty_grad_scaler
-            g_t = total_grad_flat_weighted
-            cos_LP = F.cosine_similarity(g_L, g_P, dim=0)
-            cos_tL = F.cosine_similarity(g_t, g_L, dim=0)
-            cos_tP = F.cosine_similarity(g_t, g_P, dim=0)
-            dominance = cos_tL - cos_tP  # >0 => loss-dominated; <0 => penalty-dominated
+            if args.debug:
+                g_L = loss_grads_final[pind]      * loss_weight      * loss_grad_scaler
+                g_P = penalty_grads_final[pind]   * penalty_weight   * penalty_grad_scaler
+                g_t = total_grad_flat_weighted
+                cos_LP = F.cosine_similarity(g_L, g_P, dim=0)
+                cos_tL = F.cosine_similarity(g_t, g_L, dim=0)
+                cos_tP = F.cosine_similarity(g_t, g_P, dim=0)
+                dominance = cos_tL - cos_tP  # >0 => loss-dominated; <0 => penalty-dominated
 
-            print(f"pind {pind} g_L norm {g_L.norm():.4e} g_P norm {g_P.norm():.4e}")
-            print(f"cos(L,P)={cos_LP.item():.4e}, cos(total,L)={cos_tL.item():.4e}, cos(total,P)={cos_tP.item():.4e}, dominance {dominance:.2f}")
+                print(f"pind {pind} g_L norm {g_L.norm():.4e} g_P norm {g_P.norm():.4e}")
+                print(f"cos(L,P)={cos_LP.item():.4e}, cos(total,L)={cos_tL.item():.4e}, cos(total,P)={cos_tP.item():.4e}, dominance {dominance:.2f}")
 
             if p.grad is None:
                 p.grad  = total_grad_flat_weighted.view(p.shape)
