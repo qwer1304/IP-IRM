24f4725f59ee98abdb881a0664c4b121862d4d5d
qwer1304 <qwer1304@hotmail.com>
2025-10-18 15:10:07 +0300

    debug bad pen grads

--- main.py
+++ main.py
@@ -899,7 +899,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
             penalty_env = penalty_calculator.penalty_finalize(penalty_aggregator, halves_sz) # normalized per env for macro-batch, unweighted
             for pind in range(len(penalty_grads)):
                 dPenalty_dTheta_env = penalty_grads[pind]  # per env sum of dPenalty/dTheta over macro-batch per parameter, unweighted, shape (I,J,K,param_numel)
-                print(1,pind, penalty_grads[pind].norm())
+                print(1,pind, penalty_grads[pind].norm(), dPenalty_dTheta_env.norm())
                 pen = penalty_calculator.penalty_finalize(penalty_aggregator, halves_sz, keep_halves=True)
                 total_grad_flat     = \
                     penalty_calculator.penalty_grads_finalize(
@@ -907,7 +907,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
                         pen, 
                         halves_sz,
                     ) 
-                print(2,pind, penalty_grads[pind].norm())
+                print(2,pind, penalty_grads[pind].norm(), dPenalty_dTheta_env.norm())
                 penalty_grads_final.append(total_grad_flat.detach().clone())
             p_grads_flat_weighted = torch.cat([g.detach().clone() for g in penalty_grads_final if g is not None]) * penalty_weight 
             penalty_grad_norm_weighted = p_grads_flat_weighted.norm()
