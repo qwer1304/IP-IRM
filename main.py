af5e36d0011f1632743643215e7bda8c17cab4ee
qwer1304 <qwer1304@hotmail.com>
2025-10-18 12:54:01 +0300

    debug bad pen grads

--- main.py
+++ main.py
@@ -894,12 +894,10 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
             loss_grad_norm_weighted = torch.tensor(0., dtype=torch.float, device=device)
 
         if do_penalty:
-            print()
             penalty_grads_final = []
             penalty_env = penalty_calculator.penalty_finalize(penalty_aggregator, halves_sz) # normalized per env for macro-batch, unweighted
             for pind in range(len(penalty_grads)):
                 dPenalty_dTheta_env = penalty_grads[pind]  # per env sum of dPenalty/dTheta over macro-batch per parameter, unweighted, shape (I,J,K,param_numel)
-                print("dPenalty_dTheta_env", pind, dPenalty_dTheta_env.norm())
                 total_grad_flat     = \
                     penalty_calculator.penalty_grads_finalize(
                         dPenalty_dTheta_env, 
@@ -1036,14 +1034,14 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
                                        )
 
             g_L = loss_grads_final[pind]      * loss_weight      * loss_grad_scaler
-            g_P = penalty_grads_final[pind]   * penalty_weight   * penalty_grad_scaler
+            g_P = penalty_grads_final[pind]#   * penalty_weight   * penalty_grad_scaler
             g_t = total_grad_flat_weighted
             cos_LP = F.cosine_similarity(g_L, g_P, dim=0)
             cos_tL = F.cosine_similarity(g_t, g_L, dim=0)
             cos_tP = F.cosine_similarity(g_t, g_P, dim=0)
             dominance = cos_tL - cos_tP  # >0 => loss-dominated; <0 => penalty-dominated
 
-            #print(f"pind {pind} g_L norm {g_L.norm():.4e} g_P norm {g_P.norm():.4e}")
+            print(f"pind {pind} g_L norm {g_L.norm():.4e} g_P norm {g_P.norm():.4e}")
             #print(f"cos(L,P)={cos_LP.item():.4e}, cos(total,L)={cos_tL.item():.4e}, cos(total,P)={cos_tP.item():.4e}, dominance {dominance:.2f}")
 
             if p.grad is None:
