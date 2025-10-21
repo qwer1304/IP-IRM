ef2d63e318fa7b32cc8c0782d11620e8715cdd2c
qwer1304 <qwer1304@hotmail.com>
2025-10-21 12:16:12 +0300

    debug

--- main.py
+++ main.py
@@ -1090,7 +1090,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
         l_grad0 = loss_grads_final_weighted[0].sum((0,1))[0] 
         l_grad1 = loss_grads_final_weighted[0].sum((0,1))[1] 
 
-        print(f"norms 0 {p_grad0+l_grad0.norm()} 1 {p_grad1 + l_grad1.norm()}") 
+        print(f"norms 0 {(p_grad0+l_grad0).norm()} 1 {(p_grad1 + l_grad1).norm()}") 
         exit(1)
         
         Loss_grads_flat_weighted = [loss_keep_grads_final_weighted[p] + loss_grads_final_weighted[p] for p in range(len(loss_grads_final_weighted))]
