dd2d115da453a7c636899439dadbc02444e2a8b2
qwer1304 <qwer1304@hotmail.com>
2025-11-06 10:46:51 +0200

    mocoenv

--- main.py
+++ main.py
@@ -1014,6 +1014,9 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
                                     differentiate_this.append(losses_samples)
                                     loss = losses_samples.detach()
                                 else:
+                                    if batch_index >= 1:
+                                        print()
+                                        print(idxs, loss_samples, reduction)
                                     loss = losses_samples[idxs].sum(dim=0).detach()
                                 loss_aggregator[j,partition_num,env] += loss # unnormalized, before penalty scaler
                             if do_penalty:
