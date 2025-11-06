58e0ea2f89f65c35f5cafcefe24977f04b15773a
qwer1304 <qwer1304@hotmail.com>
2025-11-06 09:35:28 +0200

    mocoenv

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ class FeatureQueue:
             n = self.queue_size
         else: 
             assert (n <= self.queue_size) and (n > 0)
-        assert (idx and (self.indices is not None)), f"idx {idx} self.indices {self.indices}"          
+        assert (idx and (self.indices is not None)) or (not idx), f"idx {idx} self.indices {self.indices}"          
         
         if self.read_ptr + n <= self.queue_size:
             indices = torch.arange(self.read_ptr, self.read_ptr+n)
