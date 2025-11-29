e2f659866cabd9088356f13b97fd5e2b62364dd5
qwer1304 <qwer1304@hotmail.com>
2025-11-29 16:38:14 +0200

    improve making splits

--- make_train_val_splits.py
+++ make_train_val_splits.py
@@ -97,8 +97,7 @@ def prune_datasets(counts, min_count=0, p=40, extreme_ratio=5, p_floor=0):
             # Subsample dominant domain to next largest domain count
             second_largest = other_counts.max()
             for d in range(D):
-                if cls_counts[d] == max_count:
-                    cls_counts[d] = extreme_ratio * second_largest
+                cls_counts[d] = min(cls_counts[d], extreme_ratio * second_largest)
 
         # Cap by p-th percentile
         nonzero_counts = cls_counts[cls_counts > p_floor]
