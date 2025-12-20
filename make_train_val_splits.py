8718865153473f0ea3134604e6b904701d139a6c
qwer1304 <qwer1304@hotmail.com>
2025-12-20 16:35:14 +0200

    single domain

--- make_train_val_splits.py
+++ make_train_val_splits.py
@@ -154,6 +154,7 @@ def prune_domains(domains, classes, raw, train_fraction=0.8, lp_train_target_per
         M_c = min(LP_TRAIN_TARGET_PER_CLASS, total_available)
         effective_M[c] = M_c
 
+    print(effective_M)
     if do_trim:
         for c in range(C):
             while True:
