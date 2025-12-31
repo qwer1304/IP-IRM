d794e9c0fa6a704586ee52140732702a85646853
qwer1304 <qwer1304@hotmail.com>
2025-12-31 14:33:50 +0200

    select a partition to test

--- linear.py
+++ linear.py
@@ -262,7 +262,7 @@ def train_val(net, data_loader, train_optimizer, batch_size, args, dataset="test
             if (net.module.updated_split_all is not None) and (args.partition_to_test is not None) and (index is not None):
                 w = net.module.updated_split_all[args.partition_to_test][index]
                 w = F.softmax(w, dim=-1)
-                mask_k = (w[:, 0] > 0.9)   # or argmax if hard
+                mask_k = (w[:, 0] > 0.9).to('cpu')   # or argmax if hard
             else:
                 mask_k = torch.ones_like(target, dtype=torch.bool)
 
