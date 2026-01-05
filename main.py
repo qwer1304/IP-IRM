7a03c64fc86330e7c9e940084c0cc2592851b9b5
qwer1304 <qwer1304@hotmail.com>
2026-01-05 16:41:47 +0200

    make loss_keep type separable

--- main.py
+++ main.py
@@ -1403,12 +1403,12 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, args, 
                 # 'grads_all' is a tuple w/ an entry per parameter.
                 # each entry is a tensor w/ 1st dim = 'grad_outputs.size(0)' and other dims matching the parameter
 
-                """
+                #"""
                 print()
                 print(f"num_samples {num_samples}, num_split_repeates {num_split_repeates}, num_baseline_repeates {num_baseline_repeates}, " +                                  
-                      f"num_repeats {num_repeats}, num_grads {num_grads}, " + 
+                      f"num_repeats {num_repeats}, num_grads {num_grads}, number_of_columns {number_of_columns}, " + 
                       f"grad_outputs {grad_outputs.size()}, differentiate_this {differentiate_this.size()}")
-                """
+                #"""
 
                 # autograd sums all gradients in each row for each parameter
                 grads_all = torch.autograd.grad(
