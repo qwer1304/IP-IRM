64227c5dca7f09ffd27540de0aad9827cca7852f
qwer1304 <qwer1304@hotmail.com>
2026-01-13 16:16:45 +0200

    integrate multiarm model

--- main.py
+++ main.py
@@ -756,10 +756,10 @@ if __name__ == '__main__':
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
             
-        #"""
+        """
         cuda_rng_state = torch.cuda.get_rng_state_all() if torch.cuda.is_available() else None
         utils.atomic_save({
-            'epoch':                74, # restore is from epoch+1
+            'epoch':                start_epoch-1, # restore is from epoch+1
             'state_dict':           model.state_dict(),
             'best_acc1':            best_acc1,
             'best_epoch':           best_epoch,
@@ -779,7 +779,7 @@ if __name__ == '__main__':
             'ema':                  ema,
         }, False, filename='{}/{}/checkpoint_multiarm2.pth.tar'.format(args.save_root, args.name))
         exit(1)
-        #"""
+        """
         
     # training loop
     # start epoch is what the user provided, if provided, or from checkpoint, if exists, or 1 (default)
