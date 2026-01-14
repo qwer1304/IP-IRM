32bbbd6ec020289f386c69ccda2a283a93228486
qwer1304 <qwer1304@hotmail.com>
2026-01-14 13:11:03 +0200

    multiarm

--- main.py
+++ main.py
@@ -764,32 +764,7 @@ if __name__ == '__main__':
             resumed = True
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
-            
-        """
-        cuda_rng_state = torch.cuda.get_rng_state_all() if torch.cuda.is_available() else None
-        utils.atomic_save({
-            'epoch':                start_epoch-1, # restore is from epoch+1
-            'state_dict':           model.state_dict(),
-            'best_acc1':            best_acc1,
-            'best_epoch':           best_epoch,
-            'optimizer':            optimizer.state_dict(),
-            'updated_split':        updated_split,
-            'updated_split_all':    updated_split_all,
-            'state_dict_momentum':  model_momentum.state_dict() if model_momentum else None,
-            'queue':                queue,
-            'state_dict_gradnorm':  gradnorm_balancer.state_dict(),
-            'gradnorm_optimizer':   gradnorm_optimizer.state_dict(),
-            "rng_dict": {
-                "rng_state":        torch.get_rng_state(),
-                "cuda_rng_state":   cuda_rng_state,
-                "numpy_rng_state":  np.random.get_state(),
-                "python_rng_state": random.getstate(),
-            },
-            'ema':                  ema,
-        }, False, filename='{}/{}/checkpoint_multiarm2.pth.tar'.format(args.save_root, args.name))
-        exit(1)
-        """
-        
+                    
     # training loop
     # start epoch is what the user provided, if provided, or from checkpoint, if exists, or 1 (default)
     start_epoch = args.start_epoch if args.start_epoch else start_epoch
