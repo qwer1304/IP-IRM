733ab3819d12a199269dd1d8ec02e782d397135c
qwer1304 <qwer1304@hotmail.com>
2026-01-30 12:35:24 +0200

    debug

--- trainenv.py
+++ trainenv.py
@@ -1590,7 +1590,9 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
                             # Need to filter the samples s.t. the samples in micro-batch are ONLY those which class==partition
                             # Use 'assign_idxs_multi' to break ties correctly
                             env_idxs = utils.assign_idxs_multi(indexs, partition, env)
+                            print(f"par {partition_num} env {env} # env_idxs {len(env_idxs}")
                             idxs = loss_module.filter_indices(env_idxs, labels=labels[env_idxs], partition=active_partition_idx[partition_num], env=env)
+                            print(f"par {partition_num{ env {env} # filtered idxs {len(idxs}")
 
                             if (N := len(idxs)) == 0:
                                 continue
