5e0c5cb62ba03db1901dd0ec05ecc5186cfacb29
qwer1304 <qwer1304@hotmail.com>
2026-02-01 14:42:04 +0200

    don't print sum(counts) for Gumbel

--- trainenv.py
+++ trainenv.py
@@ -1939,7 +1939,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
                    f" shared_cos: lk {info_dict['shared_cos_lk']:.3e} lp {info_dict['shared_cos_lp']:.3e} kp {info_dict['shared_cos_kp']:.2e}" + \
                    f" Lp: shared cos {info_dict['shared_cos_Lp']:.3e} shared dot {info_dict['shared_dot_Lp']:.3e}" + \
                    f" sparsity {args.mask_nonlinearity}: ngs2 {loss_mask_sparsity_norm**2:.2e} sum(activation) {mask_activation:.3e}" + \
-                   f" {'sum(mask)' net.module.mask_fun.mask.sum().item() if args.mask_nonlinearity != 'gumbel' else ''}" + \
+                   f" {'sum(mask) '+str(net.module.mask_fun.mask.sum().item()) if args.mask_nonlinearity != 'gumbel' else ''}" + \
                    f" gn_prgrs {info_dict['gradnorm_progress']:.6g}"
         desc_str += loss_module.get_debug_info_str()
         train_bar.set_description(desc_str)
