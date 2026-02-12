f604a99f53187f2c38e827cf2ee6594f099daee8
qwer1304 <qwer1304@hotmail.com>
2026-02-12 07:56:20 +0200

    add test knn; don't normalize features after mask in test

--- main_eqinv.py
+++ main_eqinv.py
@@ -690,7 +690,7 @@ if __name__ == '__main__':
     parser.add_argument('--test_freq', default=5*5, type=int, metavar='N',
                     help='test epoch freqeuncy')
 
-    parser.add_argument('--evaluate', type=str, default=None, nargs="*", choices=['train', 'val', 'test'], help='only evaluate')
+    parser.add_argument('--evaluate', type=str, default=None, nargs="*", choices=['train', 'val', 'test', 'knn', 'masked'], help='only evaluate')
     parser.add_argument('--extract_features', action="store_true", help="extract features for post processing during evaluate")
 
     parser.add_argument('--opt', choices=['Adam', 'SGD'], default='Adam', help='Optimizer to use')
