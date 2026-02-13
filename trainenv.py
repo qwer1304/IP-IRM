bb62093b40d01a79c20d135aeb52b034648276d5
qwer1304 <qwer1304@hotmail.com>
2026-02-13 17:43:13 +0200

    normalize w/ eps=1e-6

--- trainenv.py
+++ trainenv.py
@@ -1573,7 +1573,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
                 del batch_micro, x_1, x_2
                 torch.cuda.empty_cache()
                 
-                features_1, features_2 = F.normalize(features_1, dim=-1, eps=1e-6), F.normalize(features_2, dim=-1, eps=1e-6)
+                features_1, features_2 = F.normalize(features_1, dim=-1), F.normalize(features_2, dim=-1)
                 
                 features_1_nondetached, features_2_nondetached = mask_activation * features_1, mask_activation * features_2 
                 features_1_detached, features_2_detached = mask_activation * features_1.detach(), mask_activation * features_2.detach() 
