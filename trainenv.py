5dd540fb47c125318b22e0b2a30515805613702b
qwer1304 <qwer1304@hotmail.com>
2026-02-13 14:43:45 +0200

    debug

--- trainenv.py
+++ trainenv.py
@@ -1901,7 +1901,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
             else:
                 p.grad += total_grad_flat_weighted.view(p.shape)
                 
-            #print(f"pind {pind} name {name} norm {total_grad_flat_weighted.norm():.2e}")
+            print(f"pind {pind} name {name} norm {total_grad_flat_weighted.norm():.2e}")
                 
         
         # -----------------------
