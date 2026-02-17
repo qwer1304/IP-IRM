ce1ea135c889f9e218574da835a0a2ed967b9a50
qwer1304 <qwer1304@hotmail.com>
2026-02-17 10:50:38 +0200

    scaled gpu resize

--- resizeimgs3.py
+++ resizeimgs3.py
@@ -65,7 +65,7 @@ def main(args):
                 # Method 0 reduces CPU load during encoding for faster I/O
                 cv2.imwrite(str(dest), final_img, [
                     cv2.IMWRITE_WEBP_QUALITY, 80, 
-                    cv2.IMWRITE_WEBP_METHOD, 0
+                    64, 0 #cv2.IMWRITE_WEBP_METHOD, 0
                 ])            
 
             pbar.update(1)
