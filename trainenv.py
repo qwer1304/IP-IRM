23981f4108442259b821c4ac5e5c81e250c1238b
qwer1304 <qwer1304@hotmail.com>
2026-03-24 11:22:26 +0200

    debug

--- trainenv.py
+++ trainenv.py
@@ -1448,6 +1448,9 @@ def calculate_mask_sparsity_and_grads(mask, net, weight, do_flag, args, param_gr
 # ssl training with IP-IRM
 def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch, args, split_tags=None, **kwargs):
 
+    print()
+    print(type(partitions), len(partitions))
+    print(split_tags)
     ema = kwargs['ema']
     gradnorm_balancer, gradnorm_optimizer = kwargs['gradnorm_balancer'], kwargs['gradnorm_optimizer']
     log_file = kwargs['log_file']
