f1d4208c9ad5f717f2a4313d2da22abb894c4de8
qwer1304 <qwer1304@hotmail.com>
2026-04-03 15:10:29 +0300

    fix logging of masks

--- trainenv.py
+++ trainenv.py
@@ -2166,7 +2166,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
                 with torch.no_grad():
                     # If this number is decreasing, your sparsity IS working.
                     p_tau = p / args.tau
-                    actual_sum = (p_tau.abs().sum().item()
+                    actual_sum = p_tau.abs().sum().item()
                     print(f"L1 Norm (Sum of |Mask/tau|): {actual_sum:.12f}, # > 0.5: {(p/tau > 0.5).sum().item()}, Min mask: {p_tau.min().item()}, Max mask: {p_tau.max().item()}")
 
             """
