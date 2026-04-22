6dd3c5901b368e84dac612fe88807059dd681ef6
qwer1304 <qwer1304@hotmail.com>
2026-04-22 09:10:02 +0300

    improve logs

--- trainenv.py
+++ trainenv.py
@@ -2385,7 +2385,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
                 penalty_partitions_str = " ".join([f"penpar{i} {x:.2e}" for i, x in enumerate(total_pen_loss_partitions.tolist())])
                 cv_partitions_str = " ".join([f"cvpar{i} {x:.2e}" for i, x in enumerate(cv_partitions.tolist())])
                 
-                partitions_metrics_str = f"`Partitions: loss {loss_partitions_str}" + f"`penalty {penalty_partitions_str}" + f"`cv {cv_partitions_str}"               
+                partitions_metrics_str = f"`Partitions: loss {loss_partitions_str} " + f"penalty {penalty_partitions_str} " + f"cv {cv_partitions_str}"               
             else:
                 cv_str = ""
                 partitions_metrics_str = ""
@@ -2449,8 +2449,8 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
         
         
         cos_Lp = f"cos_p {info_dict['cos_Lp']:.3e}" if info_dict['cos_Lp'] != 0. else ""
-        dot_lp = f"{ info_dict['dot_Lp']:.3e}" if info_dict['dot_Lp'] != 0. else ""
-        Lp_str = cos_Lp + dot_lp
+        dot_lp = f"{info_dict['dot_Lp']:.3e}" if info_dict['dot_Lp'] != 0. else ""
+        Lp_str = cos_Lp + ' ' + dot_lp
         Lp_str = f"`Lp: {Lp_str}" if Lp_str != "" else ""
         Lp_shared_cos = f"shared cos {info_dict['shared_cos_Lp']:.3e}" if info_dict['shared_cos_Lp'] != 0. else ""
         Lp_shared_dot = f" shared dot {info_dict['shared_dot_Lp']:.3e}" if info_dict['shared_dot_Lp'] != 0. else ""
