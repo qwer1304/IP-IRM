5a212c448498179b2d0e7fe6f5234a64b8d4a789
qwer1304 <qwer1304@hotmail.com>
2026-05-02 08:02:42 +0300

    debug

--- trainenv.py
+++ trainenv.py
@@ -1714,9 +1714,9 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
     # soft
     mask_activation_no_threshold = net.module.mask_fun.activation(u=mask_activation_noise, use_threshold=False).detach()
     epsilon = 1e-9  # The gate to isolate 0.0
-    # Define custom edges - We want: [0.0, epsilon, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    # Define custom edges - We want: [-epsilon, epsilon, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     std_edges = torch.arange(1, 11, device=device) * 0.1
-    bin_boundaries = torch.cat([torch.tensor([epsilon], device=device), std_edges])
+    bin_boundaries = torch.cat([torch.tensor([-epsilon, epsilon], device=device), std_edges])
     bin_sets = [[0], [1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
     bin_sets = [torch.tensor(bin_set, device=device) for bin_set in bin_sets]
     prev_mask_bin_sets_epoch = partition_mask_to_bin_sets(mask_activation_no_threshold, bin_boundaries, bin_sets)
@@ -2346,12 +2346,7 @@ def train_env(net, train_loader, train_optimizer, partitions, batch_size, epoch,
                 mask_activation_no_threshold = net.module.mask_fun.activation(u=mask_activation_noise, use_threshold=False).clone().detach()
                 mask_bin_sets = partition_mask_to_bin_sets(mask_activation_no_threshold, bin_boundaries, bin_sets)
                 jaccards_epoch, jaccards_batch = [], []
-                print()
                 for i in range(len(bin_sets)):
-                    print(i)
-                    print(bin_boundaries)
-                    print(mask_bin_sets[i])
-                    print(prev_mask_bin_sets_epoch[i])
                     jaccards_epoch.append(Jaccard(mask_bin_sets[i], prev_mask_bin_sets_epoch[i]))
                     jaccards_batch.append(Jaccard(mask_bin_sets[i], prev_mask_set_batch[i] if prev_mask_set_batch is not None else prev_mask_bin_sets_epoch[i]))
                 prev_mask_set_batch = copy(mask_bin_sets)
